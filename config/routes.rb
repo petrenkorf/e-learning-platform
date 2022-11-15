d3e3fab00bd56abdf20e54f5aaf29c327f574ca6
Petris Fernandes <petrisrf@gmail.com>
2022-11-15 20:02:43 -0300

    Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: "static_pages#landing_page"
+
+  get 'static_pages/landing_page'
+  get 'static_pages/privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
